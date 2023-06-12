0d6e860b60914e9314808ae25f77e95ad1a5f1a3
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2023-06-12 16:25:45 -0400

    Use -I to include headers

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup, Extension
 clp_four_byte_encoder: Extension = Extension(
     name="clp_ffi_py.CLPFourByteEncoder",
     language="c++",
+    include_dirs=[
+        "src"
+    ],
     sources=[
         "src/clp/components/core/src/ffi/ir_stream/encoding_methods.cpp",
         "src/clp/components/core/src/ffi/encoding_methods.cpp",
         "src/clp/components/core/src/string_utils.cpp",
         "src/clp/components/core/src/TraceableException.cpp",
-        "src/clp_ffi/modules/clp_four_byte_encoder.cpp",
-        "src/clp_ffi/encoder/encoding_methods.cpp"
+        "src/clp_ffi_py/modules/clp_four_byte_encoder.cpp",
+        "src/clp_ffi_py/encoder/encoding_methods.cpp"
     ],
     extra_compile_args=[
         '-std=c++17',
