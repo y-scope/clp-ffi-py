467dea8d6edae80794f4459ad8085533eee11a9e
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2023-06-13 15:37:57 -0400

    Update cpp doc string

--- src/clp_ffi_py/encoder/encoding_methods.hpp
+++ src/clp_ffi_py/encoder/encoding_methods.hpp
@@ -5,14 +5,14 @@
 
 namespace clp_ffi_py::encoder::four_byte_encoding {
 /**
- * Encodes the preamble
+ * Encodes the preamble using 4-byte encoding
  * @param[in] self
  * @param[in] args the reference timestamp, the timestamp format, and the time
  * zone are expected in sequence
  * @return A Python bytearray containing the encoded preamble on success.
  *  If there is a failure in argument parsing or
  *  ffi::ir_stream::four_byte_encoding::encode_preamble, it returns nullptr and
- *  set PyErr accordingly.
+ *  set PyErr accordingly
  */
 auto encode_preamble(PyObject* self, PyObject* args) -> PyObject*;
 
@@ -25,9 +25,7 @@ auto encode_preamble(PyObject* self, PyObject* args) -> PyObject*;
  *  delta on success. If there is a failure in argument parsing,
  *  ffi::ir_stream::four_byte_encoding::encode_message,
  *  or ffi::ir_stream::four_byte_encoding::encode_timestamp, it returns nullptr
- *  and sets PyErr accordingly.
- *  - #clp_ffi_py::error_messages::encoder::cMessageError
- *  - #clp_ffi_py::error_messages::encoder::cTimestampError
+ *  and sets PyErr accordingly
  */
 auto encode_message_and_timestamp_delta(PyObject* self, PyObject* args) -> PyObject*;
 
@@ -38,7 +36,7 @@ auto encode_message_and_timestamp_delta(PyObject* self, PyObject* args) -> PyObj
  * @return A Python bytearray containing the encoded message on success.
  *  If there is a failure in argument parsing or
  *  ffi::ir_stream::four_byte_encoding::encode_message, it returns nullptr
- *  and sets PyErr accordingly.
+ *  and sets PyErr accordingly
  */
 auto encode_message(PyObject* self, PyObject* args) -> PyObject*;
 
@@ -49,7 +47,7 @@ auto encode_message(PyObject* self, PyObject* args) -> PyObject*;
  * @return A Python bytearray containing the encoded timestamp delta on success.
  *  If there is a failure in argument parsing or
  *  ffi::ir_stream::four_byte_encoding::encode_timestamp, it returns nullptr
- *  and sets PyErr accordingly.
+ *  and sets PyErr accordingly
  */
 auto encode_timestamp_delta(PyObject* self, PyObject* args) -> PyObject*;
 } // namespace clp_ffi_py::encoder::four_byte_encoding
