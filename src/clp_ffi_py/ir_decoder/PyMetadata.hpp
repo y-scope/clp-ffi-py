cea384cea7346691044d69b2df07ea235d01984c
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2023-07-02 04:25:35 +0000

    Add string doc for PyMetadata header

--- src/clp_ffi_py/ir_decoder/PyMetadata.hpp
+++ src/clp_ffi_py/ir_decoder/PyMetadata.hpp
@@ -8,9 +8,10 @@
 
 namespace clp_ffi_py::ir_decoder {
 /**
- * A struct that represents Python level Metadata object. This struct operates
- * as a Python Object in the Python runtime, and has the actual data stored in
- * the heap memory pointed by metadata.
+ * A PyObject structure functioning as a Python-compatible interface to retrieve
+ * CLP IR metadata. The underlying data is pointed by `metadata`. Additionally,
+ * it retains a tzinfo object at the Python level that signifies the
+ * corresponding timezone.
  */
 struct PyMetadata {
     PyObject_HEAD;
@@ -18,9 +19,38 @@ struct PyMetadata {
     PyObject* Py_timezone;
 };
 
-auto PyMetadata_init_from_json(nlohmann::json const& metadata, bool is_four_byte_encoding)
-        -> PyMetadata*;
-auto PyMetadata_module_level_init(PyObject* py_module, std::vector<PyObject*>& object_list) -> bool;
+/**
+ * Gets the PyMetadata's Python type. This type is dynamically created
+ * and initialized during the execution of `PyMetadata_module_level_init`.
+ * @return Python type object associated with PyMetadata.
+ */
 auto PyMetadata_get_PyType() -> PyTypeObject*;
+
+/**
+ * Creates and initializes PyMetadata as a Python type, and then incorporates
+ * this type as a Python object into the py_module module.
+ * @param py_module This is the Python module where the initialized PyMetadata
+ * will be incorporated.
+ * @param new_object_append_list This vector is responsible for appending all
+ * successfully created PyObjects during initialization for reference tracking
+ * purposes.
+ * @return true on success.
+ * @return false on failure with the relevant Python exception and error set.
+ */
+auto PyMetadata_module_level_init(
+        PyObject* py_module,
+        std::vector<PyObject*>& new_object_append_list) -> bool;
+
+/**
+ *
+ * @param metadata CLP IR metadata stored in JSON format.
+ * @param is_four_byte_encoding Indicates whether the CLP IR uses four-byte
+ * encoding (true) or eight-byte encoding (false).
+ * @return a new reference of a PyMetadata object that is initialized with the
+ * given input.
+ * @return nullptr on failure with the relevant Python exception and error set.
+ */
+auto PyMetadata_new_from_json(nlohmann::json const& metadata, bool is_four_byte_encoding)
+        -> PyMetadata*;
 } // namespace clp_ffi_py::ir_decoder
 #endif
