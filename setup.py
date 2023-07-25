6f8d8d58550296c9341c6ac64a7ae49a59eb4cc0
LittleStar <59785146+LinZhihao-723@users.noreply.github.com>
2023-07-24 20:47:48 -0400

    Rewrite the build logic to automate macos build and version grepping

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import os
+import sys
+import toml
 from setuptools import setup, Extension
+from typing import Any, Dict, Optional
 
 ir: Extension = Extension(
     name="clp_ffi_py.ir",
     language="c++",
-    include_dirs=[
-        "src"
-    ],
+    include_dirs=["src"],
     sources=[
         "src/clp/components/core/src/ffi/ir_stream/decoding_methods.cpp",
         "src/clp/components/core/src/ffi/ir_stream/encoding_methods.cpp",
         "src/clp/components/core/src/ffi/encoding_methods.cpp",
         "src/clp/components/core/src/string_utils.cpp",
         "src/clp/components/core/src/TraceableException.cpp",
-
         "src/clp_ffi_py/ir/encoding_methods.cpp",
         "src/clp_ffi_py/ir/Metadata.cpp",
         "src/clp_ffi_py/ir/PyFourByteEncoder.cpp",
@@ -24,18 +24,31 @@ ir: Extension = Extension(
         "src/clp_ffi_py/utils.cpp",
     ],
     extra_compile_args=[
-        '-std=c++17',
+        "-std=c++17",
         "-O3",
     ],
-    define_macros=[
-        ("SOURCE_PATH_SIZE", str(len(os.path.abspath("./src/clp/components/core"))))
-    ]
+    define_macros=[("SOURCE_PATH_SIZE", str(len(os.path.abspath("./src/clp/components/core"))))],
 )
 
-setup(
-    name="clp_ffi_py",
-    description="CLP FFI Python Interface",
-    ext_modules=[ir],
-    packages=["clp_ffi_py"],
-    version="0.0.1"
-)
+if "__main__" == __name__:
+    try:
+        if hasattr(os, "sys") and "darwin" == os.sys.platform:
+            target: Optional[str] = os.environ.get("MACOSX_DEPLOYMENT_TARGET")
+            if None is target or float(target) < 10.15:
+                os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.15"
+
+        config: Dict[str, Any] = toml.load("pyproject.toml")
+        version: Optional[str] = config.get("project", {}).get("version", None)
+        if None is version:
+            sys.exit("Error: The version number is not found in pyproject.toml")
+
+        setup(
+            name="clp_ffi_py",
+            description="CLP FFI Python Interface",
+            ext_modules=[ir],
+            packages=["clp_ffi_py"],
+            version=version,
+        )
+
+    except Exception as e:
+        sys.exit(f"Error: {e}")
