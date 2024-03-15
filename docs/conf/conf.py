67592fa7be78451ade1dc8e1371eccce54dec8fc
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-03-15 17:19:04 -0400

    docs: Switch theme from furo to pydata-sphinx-theme; Remove tippy extension. (#49)

--- docs/conf/conf.py
+++ docs/conf/conf.py
@@ -2,6 +2,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "clp-ffi-py"
+# NOTE: We don't include a period after "Inc" since the theme adds one already.
+copyright = "2023-2024 YScope Inc"
 
 # -- General configuration -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -9,31 +11,10 @@ project = "clp-ffi-py"
 extensions = [
     "myst_parser",
     "sphinx_design",
-    "sphinx_tippy",
     "sphinx.ext.viewcode",
     "sphinx.ext.autodoc",
     "sphinx_copybutton",
 ]
-
-templates_path = ["_templates"]
-
-# -- HTML output options -------------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_favicon = "https://docs.yscope.com/_static/favicon.ico"
-html_title = "clp-ffi-py"
-html_show_copyright = False
-html_static_path = ["../src/_static"]
-
-html_theme = "furo"
-
-# -- Furo theme options --------------------------------------------------------
-# https://furo.readthedocs.io/customisation/index.html
-html_theme_options = {
-    "source_repository": "https://github.com/y-scope/clp-ffi-py/",
-    "source_branch": "main",
-    "source_directory": "docs",
-}
 
 # -- MyST extensions -----------------------------------------------------------
 # https://myst-parser.readthedocs.io/en/stable/syntax/optional.html
@@ -47,3 +28,43 @@ myst_enable_extensions = [
 autoclass_content = "class"
 autodoc_class_signature = "separated"
 autodoc_typehints = "description"
+
+# -- HTML output options -------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_favicon = "https://docs.yscope.com/_static/favicon.ico"
+html_title = "clp-ffi-py"
+html_show_copyright = True
+
+html_static_path = ["../src/_static"]
+
+html_theme = "pydata_sphinx_theme"
+
+# -- Theme options -------------------------------------------------------------
+# https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/layout.html
+
+html_theme_options = {
+    "footer_start": ["copyright"],
+    "footer_center": [],
+    "footer_end": ["theme-version"],
+    "primary_sidebar_end": [],
+    "secondary_sidebar_items": ["page-toc", "edit-this-page"],
+    "show_prev_next": False,
+    "use_edit_page_button": True,
+}
+
+# -- Theme source buttons ------------------------------------------------------
+# https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/source-buttons.html
+
+html_context = {
+    "github_user": "y-scope",
+    "github_repo": "clp-ffi-py",
+    "github_version": "main",
+    "doc_path": "docs/src",
+}
+
+# -- Theme custom CSS and JS ---------------------------------------------------
+# https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/static_assets.html
+
+def setup(app):
+    app.add_css_file("custom.css")
