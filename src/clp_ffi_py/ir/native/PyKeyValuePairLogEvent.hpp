6f20e574e9ba40bab3986f9730b4e0868a2ce877
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-10-30 21:18:32 -0400

    Add template to start

---
+++ src/clp_ffi_py/ir/native/PyKeyValuePairLogEvent.hpp
@@ -0,0 +1,14 @@
+#ifndef CLP_FFI_PY_IR_NATIVE_PYKEYVALUEPAIRLOGEVENT_HPP
+#define CLP_FFI_PY_IR_NATIVE_PYKEYVALUEPAIRLOGEVENT_HPP
+
+namespace clp_ffi_py::ir::native {
+/**
+ * A PyObject structure functioning as a Python-compatible interface to retrieve a key-value pair
+ * log event. The underlying data is pointed to by `m_kv_pair_log_event`.
+ */
+class PyKeyValuePairLogEvent {
+
+};
+}
+
+#endif  // CLP_FFI_PY_IR_NATIVE_PYKEYVALUEPAIRLOGEVENT_HPP
