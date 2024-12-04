b0df0dc78b2110d59f2fe6d87d1298c444cbf914
Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>
2024-12-04 12:53:38 -0500

    refactor: Fix clang-tidy warnings in `utils`. (#101)

--- src/wrapped_facade_headers/msgpack.hpp
+++ src/wrapped_facade_headers/msgpack.hpp
@@ -6,7 +6,12 @@
 #ifdef CLP_FFI_PY_ENABLE_LINTING
 // Inform IWYU of the headers that we use that are exported by msgpack.hpp
 // IWYU pragma: begin_exports
-// TODO: Add necessary msgpack internal headers to silence clang-tidy warnings
+#include <msgpack/v1/object_decl.hpp>
+#include "msgpack/v1/unpack_decl.hpp"
+#include <msgpack/v2/object_decl.hpp>
+#include "msgpack/v2/unpack_decl.hpp"
+#include <msgpack/v3/object_decl.hpp>
+#include "msgpack/v3/unpack_decl.hpp"
 // IWYU pragma: end_exports
 #endif
 // clang-format on
