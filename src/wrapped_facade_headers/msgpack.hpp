fdf5cc54719e1606cf610a600f49017f27454e8d
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-01-05 18:49:15 -0500

    Add v1 and v2 APIs back

--- src/wrapped_facade_headers/msgpack.hpp
+++ src/wrapped_facade_headers/msgpack.hpp
@@ -6,6 +6,12 @@
 #ifdef CLP_FFI_PY_ENABLE_LINTING
 // Inform IWYU of the headers that we use that are exported by msgpack.hpp
 // IWYU pragma: begin_exports
+#include <msgpack/v1/object_decl.hpp>
+#include <msgpack/v1/object_fwd_decl.hpp>
+#include "msgpack/v1/unpack_decl.hpp"
+#include <msgpack/v2/object_decl.hpp>
+#include <msgpack/v2/object_fwd_decl.hpp>
+#include "msgpack/v2/unpack_decl.hpp"
 #include <msgpack/v3/object_decl.hpp>
 #include <msgpack/v3/object_fwd_decl.hpp>
 #include "msgpack/v3/unpack_decl.hpp"
