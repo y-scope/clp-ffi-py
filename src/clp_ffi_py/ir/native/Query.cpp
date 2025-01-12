301b510304742aa16c218330fb037b670b1eec60
Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>
2025-01-11 22:41:49 -0500

    refactor: Fix clang-tidy warnings in `Query`. (#117)

--- src/clp_ffi_py/ir/native/Query.cpp
+++ src/clp_ffi_py/ir/native/Query.cpp
@@ -10,16 +10,12 @@ auto Query::matches_wildcard_queries(std::string_view log_message) const -> bool
     if (m_wildcard_queries.empty()) {
         return true;
     }
-    return std::any_of(
-            m_wildcard_queries.begin(),
-            m_wildcard_queries.end(),
-            [&](auto const& wildcard_query) {
-                return clp::string_utils::wildcard_match_unsafe(
-                        log_message,
-                        wildcard_query.get_wildcard_query(),
-                        wildcard_query.is_case_sensitive()
-                );
-            }
-    );
+    return std::ranges::any_of(m_wildcard_queries, [&](auto const& wildcard_query) {
+        return clp::string_utils::wildcard_match_unsafe(
+                log_message,
+                wildcard_query.get_wildcard_query(),
+                wildcard_query.is_case_sensitive()
+        );
+    });
 }
 }  // namespace clp_ffi_py::ir::native
